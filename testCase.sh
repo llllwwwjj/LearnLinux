c65f3ade60ed8b00bdb91255912d3c260c1f0a58
知世 <1445302197@qq.com>
2022-04-20 09:20:48 +0800

    Add files via upload

---
+++ testCase.sh
@@ -0,0 +1,12 @@
+echo "Is it morning ? Please answer yes or no"
+read timeofday
+
+case $timeofday in
+	yes) echo "Good morning";;
+	no) echo  "Good afternoon";;
+	y) echo "Good morning";;
+	n) echo  "Good afternoon";;
+	*) echo "sorry ,answer not recognized";;
+esac
+
+exit 0
---
+++ testfor.sh
@@ -0,0 +1,9 @@
+for foo in "bar fud 43"
+do 
+	echo $foo
+done
+exit 0
+#for file in $(ls f*.sh);do
+#	lpr $file
+#done 
+#exit 0
